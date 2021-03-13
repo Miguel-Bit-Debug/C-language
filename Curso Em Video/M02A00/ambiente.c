2a51c58cba7a26ed8e03086fa7f1a2c333af39ee
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-03-12 23:48:58 -0300

    Add new scripts

--- Curso Em Video/M02A00/ambiente.c
+++ Curso Em Video/M02A00/ambiente.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 void main() {
-    int a = 3;
-    int b = 2;
-    int r = a + b;
+    //int a = 3;
+    //int b = 2;
+    //int r = a + b;
 
-    printf("O resultado eh %i", r);
+    //printf("O resultado eh %i", r);
+
+    int x = 5;
+    int y = 2;
+    float z = (float) x / y;
+    printf("O resultado eh: %.1f ", z);
 }
